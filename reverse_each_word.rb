a2b26cdd8e8898bec6ca64c37f148b7d76a84ccf
Petya Djanovska <petya.djanovska@gmail.com>
2018-01-02 18:38:32 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,10 @@ def reverse_each_word(string)
   array = string.split(" ")
   reversed = String.new
   array.each do |substring|
-    reversed << substring.reverse! + " " if
+    reversed << substring.reverse! + " "
+    if array.last
+      reversed << array.last.reverse!
+    end
     array.last reversed << substring.reverse!
   end
   reversed
